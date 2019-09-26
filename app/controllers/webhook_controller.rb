80c134af3990a4b0b0c64d0f4b075810cce32edf
mihhus <gi33ti51@gmail.com>
2019-09-26 20:53:18 +0900

    Change strage user_query and debug

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -17,6 +17,7 @@ class WebhookController < ApplicationController
   def callback
     body = request.body.read
     response_json = ""
+    text = ""
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
@@ -31,30 +32,27 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-=begin
           user_query = URI.escape(event.message['text'], /[^-_.!~*'()a-zA-Z\d]/u)
-          uri = URI.parse(GOOGLEAPI_ENDPOINT + "/books/v1/volumes?q=" + user_query)
-          text = ""
           books_data = []
           data_acquisition = 0
           startIndex = 0
+          # 書誌情報にISBNを持つ本の情報を10冊集めたらbreakする
           loop do
+            uri = URI.parse(GOOGLEAPI_ENDPOINT + "/books/v1/volumes?q=" + user_query + "&startIndex=" + startIndex)
             begin
               response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
                 http.get(uri.request_uri)
+                response_json = JSON.parse(response.body)
               end
-              response_json = JSON.parse(response.body)
             rescue => e
               text << "Googlegaが悪いよー"
             end
             startIndex += 1
             response_json['items'].each_with_index do |item, index|
               # ISBNが存在しなければスキップ
-              type = imte.dig('volumeInfo', 'industryIdentifiers', 'type')
+              type = item.dig('volumeInfo', 'industryIdentifiers', 'type')
               if type then
-                books_data[index][0] << item.dig('volumeInfo', 'industryIdentifiers', type)
-                books_data[index][1] = item['volumeInfo']['title']
-                books_data[index][2] = item['volumeInfo']['author']
+                books_data[index].push([item.dig('volumeInfo', 'industryIdentifiers', type), item['volumeInfo']['title'], item['volumeInfo']['author']])
                 data_acquisition += 1
                 break if data_acquisition == 10
               end
@@ -64,9 +62,7 @@ class WebhookController < ApplicationController
           if @@user_data[userId] then
             if @@user_data[userId][:location] then
               # Locationがすでに設定されている
-              endpoint = "http://api.calil.jp"
               latitude = @@user_data[userId][:location][:latitude]
-              longitude = @@user_data[userId][:location][:longitude]
               library_data = []
               uri = URI.parse(CALILAPI_ENDPOINT + "/library?appkey=#{calil_appkey}&geocode=#{longitude},#{latitude}&limit=10&format=json&callback= ")
               begin
@@ -78,8 +74,7 @@ class WebhookController < ApplicationController
                 text << "カーリルが悪いよー\n"
               end
               response_json.each_with_index do |value, index|
-                library_data[index][0] = value["systemid"]
-                library_data[index][1] = value["short"]
+                library_data.push([value["systemid"],value["short"]])
               end
               uri = URI.parse(endpoint + "/check?appkey=#{calil_appkey}&systemid=#{library_data.map{|row| row[0]}.join(',')}&isbn=#{books_data.map{|row| row[0]}.join('')}&format=json&callback=no")
               begin
@@ -91,9 +86,8 @@ class WebhookController < ApplicationController
                 text << "カーリルが悪いよー\n"
               end
               # 図書館ごとの応答を吸収するためにcalilAPI側にpollingが実装されているその対応を書く
-              while response_json["continue"] == 1
+              while response_json["continue"] == 1 do
                 # pollingが始まるとjsonp形式でのみ返答となるので整形してからデータを扱う, 配列内部にJSONが格納されていることに注意が必要
-                # polling中に適宜情報をクライアントに提示する機能は実装しない
                 uri = URI.parse(endpoint + "/check?appkey=#{calil_appkey}&session#{response["session"]}&format=json")
                 begin
                   response = Net::HTTP.start(uri.host, uri.port) do |http|
@@ -115,13 +109,12 @@ class WebhookController < ApplicationController
               @@user_data[userId][:user_query] = user_query
               text << "位置情報を入力してね\n"
             end
-=end
-            text = "test"
-            message = {
-              type: 'text',
-              text: text
-            }
-            client.reply_message(event['replyToken'], message)
+          end
+          message = {
+            type: 'text',
+            text: text
+          }
+          client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
