1c911451367e19e6333d79cbf7035a8481589831
mihhus <gi33ti51@gmail.com>
2019-09-26 21:16:01 +0900

    debug change to use dig for JSON when diging ISBN data

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -47,16 +47,19 @@ class WebhookController < ApplicationController
             rescue => e
               text << "Googlegaが悪いよー"
             end
-            startIndex += 1
             response_json['items'].each do |item|
               # ISBNが存在しなければスキップ
-              type = item.dig('volumeInfo', 'industryIdentifiers', 'type')
-              if type =="ISBN_10" or type == "ISBN_13" then
-                books_data.push([item.dig('volumeInfo', 'industryIdentifiers', 'identifier'), item['volumeInfo']['title'], item['volumeInfo']['author']])
-                data_acquisition += 1
-                break if data_acquisition == 10
+              if type = item.dig('volumeInfo', 'industryIdentifiers') then
+                if type = type[0].dig('type') then
+                  if type =="ISBN_10" or type == "ISBN_13" then
+                    books_data.push([item.dig('volumeInfo', 'industryIdentifiers', 'identifier'), item['volumeInfo']['title'], item['volumeInfo']['author']])
+                    data_acquisition += 1
+                    break if data_acquisition == 10
+                  end
+                end
               end
             end
+            startIndex += 1
           end
           # 書籍のデータが何件あるかで条件を分岐したい(仮)
 =begin
