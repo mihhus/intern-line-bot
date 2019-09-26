277fa40eff1d02752c98dcd2f23db00669ef3911
mihhus <gi33ti51@gmail.com>
2019-09-26 11:57:13 +0900

    Collect 10 books with isbn

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -5,6 +5,7 @@ require 'json'
 
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
+  @@user_data = []
 
   def client
     @client ||= Line::Bot::Client.new { |config|
@@ -21,8 +22,10 @@ class WebhookController < ApplicationController
       head 470
     end
 
+
     events = client.parse_events_from(body)
     events.each { |event|
+      userId = event['source']['userId']
       case event
       when Line::Bot::Event::Message
         case event.type
@@ -39,14 +42,39 @@ class WebhookController < ApplicationController
           rescue => e
             p e
           end
-          response_json['items'].each do |item|
-            text << item['volumeInfo']['title'] + "\n"
+          isbn_numbers = []
+          data_acquisition = 0
+          startIndex = 0
+          loop do
+            response = JSON.parse(Net::HTTP.get(URI.parse(endpoint + "/books/v1/volumes?q=" + user_query_escape + "&startIndex=" + startIndex)))
+            startIndex += 1
+            if response_json['items'] then
+              10.times do |index|
+                if response_json['items'][index]['volumeInfo']['isbn_10'] then
+                  isbn_numbers << response_json['items'][index]['volumeInfo']['isbn_10']
+                  data_acquisition += 1
+                  break if data_acquisition == 10
+                elsif response_json['items'][index]['volumeInfo']['isbn_13'] then
+                  isbn_numbers << response_json['items'][index]['volumeInfo']['isbn_13']
+                  data_acquisition += 1
+                  break if data_acquisition == 10
+                end
+              end
+            end
           end
-          message = {
-            type: 'text',
-            text: text
-          }
-          client.reply_message(event['replyToken'], message)
+          if @@user_data[userId] then
+            if @@user_data[userId][:location] then
+              # Locationがすでに設定されている
+            else
+              @@user_data[userId][:user_query] = user_query
+              text += "位置情報を入力してね\n"
+              message = {
+                type: 'text',
+                text: text
+              }
+              client.reply_message(event['replyToken'], message)
+            end
+          end
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
@@ -83,3 +111,4 @@ private
 CALILAPI_ENDPOINT = "http://api.calil.jp"
 GOOGLEAPI_ENDPOINT = "https://www.googleapis.com"
 end
+
