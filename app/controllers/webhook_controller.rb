da12b0edb4e480f14050fa3417af85e6d0e53459
mihhus <gi33ti51@gmail.com>
2019-09-27 16:20:48 +0900

    delete author info

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -56,7 +56,7 @@ class WebhookController < ApplicationController
                 if industry.kind_of?(Hash) then
                   type = industry.dig('type')
                   if type == "ISBN_10" || type == "ISBN_13" then
-                    books_data.push([industry.dig('identifier'), item['volumeInfo']['title'], item['volumeInfo']['author']])
+                    books_data.push([industry.dig('identifier'), item['volumeInfo']['title'], item['volumeInfo']])
                     # text << industry.dig('identifier')
                     # text << "text"
                     # text << item['volumeInfo']['title']
@@ -66,7 +66,7 @@ class WebhookController < ApplicationController
                 elsif industry.kind_of?(Array) then
                   type = industry[0].dig('type')
                   if type == "ISBN_10" || type == "ISBN_13" then
-                    books_data.push([industry[0].dig('identifier'), item['volumeInfo']['title'], item['volumeInfo']['author']])
+                    books_data.push([industry[0].dig('identifier'), item['volumeInfo']['title'], item['volumeInfo']])
                     data_acquisition += 1
                   end
                 end
@@ -123,7 +123,6 @@ class WebhookController < ApplicationController
               books_data.each_with_index do |book_item, book_index|
                 # text << "title: #{books_data[book_index][1]}\n"
                 # library_data.each_with_index do |library_item, library_index|
-                  # text << "  author\n"
                   # text << "  #{library_data[library_index][1]}: #{@response_json['books'][books_data[book_index][0]]['libkey'].to_a}\n"
               end
 =end
