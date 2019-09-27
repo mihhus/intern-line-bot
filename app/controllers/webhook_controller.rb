bbb371a9d03f40fa7be292eaed87899504f2f700
mihhus <gi33ti51@gmail.com>
2019-09-27 14:53:18 +0900

    debug

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -58,7 +58,8 @@ class WebhookController < ApplicationController
                   if type == "ISBN_10" || type == "ISBN_13" then
                     books_data.push([industry.dig('identifier'), item['volumeInfo']['title'], item['volumeInfo']['author']])
                     # text << industry.dig('identifier')
-                    text << item['volumeInfo']['title']
+                    text << "text"
+                    # text << item['volumeInfo']['title']
                     # text << item['volumeInfo']['title']
                     data_acquisition += 1
                     break if data_acquisition == 10
