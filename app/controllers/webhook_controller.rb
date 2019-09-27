2d8b3034ae9b891b1ee85115382725950ba1b73a
mihhus <gi33ti51@gmail.com>
2019-09-27 14:04:22 +0900

    debug

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -116,7 +116,6 @@ class WebhookController < ApplicationController
                 end
               end
               text << "syuturyokunotoko\n"
-=begin
               books_data.each_with_index do |book_item, book_index|
                 # text << "title\n"
                 # text << "title: #{books_data[book_index][1]}\n"
@@ -125,7 +124,6 @@ class WebhookController < ApplicationController
                   # text << "  #{library_data[library_index][1]}: #{@response_json['books'][books_data[book_index][0]]['libkey'].to_a}\n"
                 end
               end
-=end
             else
               @@user_data[userId] = {:user_query => user_query}
               text << "位置情報を入力してね"
