24b47bd8f046a70e35da728bef8473dfa22c273c
mihhus <gi33ti51@gmail.com>
2019-09-27 16:25:46 +0900

    debug output

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -115,7 +115,7 @@ class WebhookController < ApplicationController
               end
               text << "syuturyokunotoko\n"
               books_data.each_with_index do |book_item, book_index|
-                # text << "title: #{books_data[book_index][1]}\n"
+                text << "title: #{books_data[book_index][1]}\n"
                 # library_data.each_with_index do |library_item, library_index|
                   # text << "  #{library_data[library_index][1]}: #{@response_json['books'][books_data[book_index][0]]['libkey'].to_a}\n"
               end
