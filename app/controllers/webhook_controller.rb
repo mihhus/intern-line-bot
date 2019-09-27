61e3797f3de26176dc01052cae7bda2f9f247837
mihhus <gi33ti51@gmail.com>
2019-09-27 16:41:46 +0900

    debug

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -117,7 +117,8 @@ class WebhookController < ApplicationController
                 break if book_index == 10
                 text << "#{book_item[1]}\n"
                 library_data.each_with_index do |library_item, library_index|
-                  text << "  #{library_item[1]}: #{@response_json['books'][book_item[0]][library_item[0]].to_a}\n"
+                  # text << "  #{library_item[1]}: #{@response_json['books'][book_item[0]][library_item[0]].to_a}\n"
+                  text << "  #{library_item[1]}: \n"
                 end
               end
             else
