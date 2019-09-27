4d290b88346dc69c6a99f9caccab277db836fa8f
mihhus <gi33ti51@gmail.com>
2019-09-27 16:49:02 +0900

    debug use dig

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -117,7 +117,7 @@ class WebhookController < ApplicationController
                 break if book_index == 3
                 text << "#{book_item[1]}\n"
                 library_data.each_with_index do |library_item, library_index|
-                  text << "  #{library_item[1]}: #{@response_json['books'][book_item[0]][library_item[0]].to_a}\n"
+                  text << "  #{library_item[1]}: #{@response_json.dig('books', book_item[0], library_item[0]).to_a}\n"
                 end
               end
             else
