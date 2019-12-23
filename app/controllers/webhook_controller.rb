edb93bd82ee76a822755830b524f72bfe2c663c9
mihhus <gi33ti51@gmail.com>
2019-12-24 01:46:54 +0900

    test

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -34,7 +34,7 @@ class WebhookController < ApplicationController
           library_data = []
           data_acquisition = 0
           startIndex = 0
-          @response_json = 0
+          @response_json = {}
           # 書誌情報にISBNを持つ本の情報を10冊集めたらbreakする
           loop do
             uri = URI.parse(GOOGLEAPI_ENDPOINT + "/books/v1/volumes?q=" + user_query + "&maxResults=10&startIndex=" + startIndex.to_s)
