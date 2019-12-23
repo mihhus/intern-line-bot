2aa56181c595266a3ccdd30007c93cf23322bda6
mihhus <gi33ti51@gmail.com>
2019-12-24 02:49:17 +0900

    test

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -64,7 +64,7 @@ class WebhookController < ApplicationController
               industry = industrys[0] if industrys.kind_of?(Array)
           message = {
             type: 'text',
-            text: "hakkyou"
+            text: item['volumeInfo']['title']
           }
           client.reply_message(event['replyToken'], message) if industrys.kind_of?(Hash)
           client.reply_message(event['replyToken'], message) if industrys.kind_of?(Array)
