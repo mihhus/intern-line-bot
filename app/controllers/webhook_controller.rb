1e2c8bca622e7b509922624bf5e480e20e819f80
mihhus <gi33ti51@gmail.com>
2019-12-24 04:23:02 +0900

    test

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -67,12 +67,14 @@ class WebhookController < ApplicationController
                   books_data.push(industry['identifier'], item['volumeInfo']['title'])
                   data_acquisition += 1
               end
-            end
+              if data_acquisition != 1 then
               message = {
                 type: 'text',
                 text: uri
               }
               client.reply_message(event['replyToken'], message)
+              end
+            end
             break if data_acquisition > 10
             startIndex += 1
           end
